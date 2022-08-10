5ccf0397b4fdb2f09aaa2688b66ed122d46e6e20
Zimarlen Silva <zimarlensilva@gmail.com>
2022-08-10 11:12:05 -0700

    second commit

--- js/index.js
+++ js/index.js
@@ -11,3 +11,16 @@ console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+
+if (hacker1.length > hacker2.length) {
+console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else if (hacker1.length === hacker2.length) {
+console.log(`Wow, you both have equally long names, ${hacker1.length} `);
+}
+
+// 2.1. Depending on which name is longer, print:
+// - The driver has the longest name, it has XX characters. or
+// - It seems that the navigator has the longest name, it has XX characters. or
+// - Wow, you both have equally long names, XX characters!.
