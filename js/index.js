95b5e35e40b92c39e26b12ecb6485b28a0db6e2e
Zimarlen Silva <zimarlensilva@gmail.com>
2022-08-10 13:38:15 -0700

    last changes

--- js/index.js
+++ js/index.js
@@ -13,14 +13,34 @@ console.log(`The navigator's name is ${hacker2}`);
 
 
 if (hacker1.length > hacker2.length) {
-console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
 } else if (hacker1.length < hacker2.length) {
-console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 } else if (hacker1.length === hacker2.length) {
-console.log(`Wow, you both have equally long names, ${hacker1.length} `);
+    console.log(`Wow, you both have equally long names, ${hacker1.length} `);
 }
 
-// 2.1. Depending on which name is longer, print:
-// - The driver has the longest name, it has XX characters. or
-// - It seems that the navigator has the longest name, it has XX characters. or
-// - Wow, you both have equally long names, XX characters!.
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+
+let driversName = " ";
+for (let i = 0; i < hacker1.length; i++) {
+    driversName += hacker1[i].toUpperCase() + " ";
+}
+console.log(driversName.trim());
+
+let navigatorsName = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    navigatorsName += hacker2[i];
+}
+console.log(navigatorsName);
+
+hacker1 = hacker1.toLowerCase();
+hacker2 = hacker2.toLowerCase();
+
+if (hacker1 > hacker2) {
+    console.log("The driver's name goes first.")
+} else if (hacker1 < hacker2) {
+    console.log("Yo, the navigator goes first definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
