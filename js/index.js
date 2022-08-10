1cba4d10222a66383dc4430714e251d8425757e3
Zimarlen Silva <zimarlensilva@gmail.com>
2022-08-10 09:21:58 -0700

    names and inputs

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 console.log("I'm ready!");
 
 // Iteration 1: Names and Input
-//
+
+
+let hacker1 = "WALTER WHITE";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "JESSE PINKMAN";
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
-
-// Iteration 3: Loops
